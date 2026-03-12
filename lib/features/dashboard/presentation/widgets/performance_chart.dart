6f494b1dd906762358eb75705935707620c40db2
Yared Mehari <yared4193@gmail.com>
2026-03-12 07:34:41 +0300

    update

--- lib/features/dashboard/presentation/widgets/performance_chart.dart
+++ lib/features/dashboard/presentation/widgets/performance_chart.dart
@@ -99,6 +99,7 @@ class ProgramPerformanceChart extends StatelessWidget {
                           case 10: text = 'NOV'; break;
                         }
                         return SideTitleWidget(
+                          meta: meta,
                           child: Text(text, style: style),
                         );
                       },
